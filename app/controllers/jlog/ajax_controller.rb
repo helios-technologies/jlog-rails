51ac194ebb96420f6b719d817c56ddcd31cd9122
dima <dlevchenko@heliostech.hk>
2012-10-18 17:28:21 +0300

    adding butch functionality to ajaxAppender

--- app/controllers/jlog/ajax_controller.rb
+++ app/controllers/jlog/ajax_controller.rb
@@ -4,16 +4,19 @@ module Jlog
     include ActionController::Rendering
 
     def append
-      message = params[:message]
-      level_pattern = /^(DEBUG|INFO|WARN|ERROR|FATAL)/
-      level = message.match(level_pattern)[1]
-      message = 'Client ' << message
+      messages = params[:message]
+      messages = [message] unless messages.is_a? Array
+      messages.each do |message|
+        level_pattern = /^(DEBUG|INFO|WARN|ERROR|FATAL)/
+        level = message.match(level_pattern)[1]
+        message = 'Client ' << message
 
-      if ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'].include? level
-        Rails.logger.send(level.downcase.to_sym, message)
-      else
-        Rails.logger.warn('*** Attempt to log with a nonexistent level ***')
-        Rails.logger.warn(message)
+        if ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'].include? level
+          Rails.logger.send(level.downcase.to_sym, message)
+        else
+          Rails.logger.warn('*** Attempt to log with a nonexistent level ***')
+          Rails.logger.warn(message)
+        end
       end
 
       render text: 'ok', status: :ok
--- jlog-rails.gemspec
+++ jlog-rails.gemspec
@@ -4,10 +4,10 @@ require File.expand_path("lib/jlog/version")
 Gem::Specification.new do |s|
   s.name        = 'jlog-rails'
   s.version     = Jlog::VERSION
-  s.date        = '2012-08-31'
+  s.date        = '2012-10-18'
   s.summary     = "Jlog for Rails"
   s.description = "A lightweight JavaScript logger (for Rails)"
-  s.authors     = ["Alexey Golubev"]
+  s.authors     = ["Alexey-высокогорный-чай-Golubev"]
   s.email       = 'oholubyev@heliostech.hk'
   s.files       = `git ls-files`.split("\n")
   s.test_files  = `git ls-files -- {test,spec,features}/*`.split("\n")
